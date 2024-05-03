0247854890fd8103d58d8f7be9f9054d15b72303
brunoslima <brunoslima4@gmail.com>
2024-05-03 17:10:48 -0300

    Add - Funções Matemáticas

---
+++ funcoes.php
@@ -0,0 +1,15 @@
+<?php
+
+function adicao($num1, $num2){
+    return($num1 + $num2);
+}
+
+function subtracao($num1, $num2){
+    return($num1 - $num2);
+}
+
+function multiplicacao($num1, $num2){
+    return($num1 * $num2);
+}
+
+?>
